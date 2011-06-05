3f40f5db1e9a83b5edd7066b2646597b8ebc3b5a
kreuzerkrieg <kreuzerkrieg@gmail.com>
2011-06-05 19:34:51 +0300

    empty logger for debugging

---
+++ CodeOrganizer/EmptyLogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeOrganizer
+{
+    class EmptyLogger : Logger
+    {
+        public void OpenLog()
+        {
+
+        }
+
+        public void PrintMessage(object oMessage)
+        {
+
+        }
+
+        public void PrintHeaderMessage(object oMessage)
+        {
+
+        }
+
+        public void CloseLog()
+        {
+
+        }
+    }
+}
