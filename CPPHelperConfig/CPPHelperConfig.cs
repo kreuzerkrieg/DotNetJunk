77f685eede0dcf7e2a610778683927c40a18aa95
Ernest <ernest.zaslavsky@gmail.com>
2011-07-28 21:29:10 +0300

    Tools Page

---
+++ CPPHelperConfig/CPPHelperConfig.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CPPHelperConfig
+{
+    public partial class CPPHelperConfig : UserControl
+    {
+        public CPPHelperConfig()
+        {
+            InitializeComponent();
+        }
+
+        private void CPPHelperConfig_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
